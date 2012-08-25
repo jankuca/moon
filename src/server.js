d74389370bfd761c3314b16e02c976d9048bc186
Jan Kuča <jan@jankuca.com>
2012-08-25 23:48:59 +0200

    improve errors

--- src/server.js
+++ src/server.js
@@ -18,8 +18,15 @@ Server.prototype.run = function () {
     d.on('error', function (err) {
       var stack = err.stack || err.message || err.toString();
       stack = stack.replace(/^(.+?:)/, '\033[0;31m$1\033[0m');
-      stack = stack.replace(/(\n\s+at\s)(\S+)/g, '$1\033[0;32m$2\033[0m');
-      stack = stack.replace(/:(\d+):/g, ':\033[4;36m$1\033[0m:');
+      stack = stack.replace(/\(.+?\)/g, function (match) {
+        var app_specific = (match.indexOf('/node_modules/') === -1 && match !== '(native)');
+        var color = app_specific ? '\033[0m' : '\033[0;37m';
+        return color + match.replace(/:(\d+):/, ':\033[4;36m$1' + color + ':') + '\033[0m';
+      });
+      stack = stack.replace(/(\n\s+at\s)([^\(]+)(\s+)/g, '$1\033[0;32m$2\033[0m$3');
+      stack = stack.replace(/(\n\s+at\s[^m]+m)([^\(]+)/g, function (match, prefix, call) {
+        return prefix + call.replace(/(^|\W)([A-Z][\w]*)/g, '$1\033[4m$2\033[0;32m');
+      });
       console.error(stack);
 
       res.writeHead(500, {
