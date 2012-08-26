d329f00264090908960b655c209a602296691f6e
Jan Kuča <jan@jankuca.com>
2012-08-26 16:42:50 +0200

    add tests for Scope

---
+++ test/scope.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var Scope = require('../src/scope');
+
+
+describe('Scope', function () {
+
+  it('should initially be empty (have no enumerable keys)', function () {
+    var scope = new Scope();
+    assert.deepEqual(Object.keys(scope), []);
+  });
+
+
+  it('should notify all associated widgets about updates', function () {
+    var MockWidget = function () {
+      this.updated = false;
+    };
+    MockWidget.prototype.update = function () {
+      this.updated = true;
+    };
+
+    var widget_a = new MockWidget();
+    var widget_b = new MockWidget();
+
+    var scope = new Scope();
+    scope.$addWidget(widget_a);
+    scope.$addWidget(widget_b);
+    scope.a = 123;
+    scope.$update();
+
+    assert(widget_a.updated);
+    assert(widget_b.updated);
+  });
+
+})
