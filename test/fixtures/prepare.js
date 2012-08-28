da297b97c1787cac9ff354d24b0e2f7eaf33bdee
Jan Kuča <jan@jankuca.com>
2012-08-28 16:38:16 +0200

    include app log/error after test results

--- test/fixtures/prepare.js
+++ test/fixtures/prepare.js
@@ -14,18 +14,40 @@ global.include = function (file_path) {
 };
 
 
-var createLogger = function (target) {
+var console_log = console.log;
+var console_error = console.error;
+var log = [];
+
+var createLogger = function (target, highlight) {
   return function () {
     var err = new Error();
     var origin = err.stack.split("\n")[2];
     if (/mocha/.test(origin)) {
       target.apply(global.console, arguments);
+
+    } else {
+      var args = [].slice.call(arguments);
+      if (args[1]) {
+        args[1] = (highlight ? '\033[0m' : '\033[0;37m') + args[1];
+      }
+
+      log.push([ target ].concat(args));
     }
   };
 };
 
 global.console.log = createLogger(global.console.log);
 global.console.info = createLogger(global.console.info);
-global.console.warn = createLogger(global.console.warn);
-global.console.error = createLogger(global.console.error);
-global.console.trace = createLogger(global.console.trace);
+global.console.warn = createLogger(global.console.warn, true);
+global.console.error = createLogger(global.console.error, true);
+global.console.trace = createLogger(global.console.trace, true);
+
+process.on('exit', function () {
+  if (log.length > 0) {
+    console_log('');
+    log.forEach(function (args) {
+      args[0].apply(global.console, args.slice(1));
+    });
+    console_log('\033[0m');
+  }
+});
